925379a3f97fad7569acac9f0d366156e6ec929a
Sean Lacy <seanl56688@gmail.com>
2024-03-07 11:53:02 -0500

    Start of Trivia App and Redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
 function App() {
   
   return (
-    <h1>This is my Website</h1>
+    <>
+    <h1>Welcome to React Trivia!!</h1>
+    <h1>Press The Start Button to Begin</h1>
+    <button type="button" className="btn btn-success">Press Me to Start</button>
+    </>
   )
 }
 
